74e8347cda29e0886a1af1ce1a2ad63239a5622b
maxxmoto <maxxmoto@users.noreply.github.com>
2026-06-27 00:38:17 +0300

    Fix: map neofamily lines to correct EGE task numbers, remove old files

--- src/tasks/index.js
+++ src/tasks/index.js
@@ -1,32 +1,51 @@
-import { tasks as t1 } from './neofamily_line1.js';
-import { tasks as t2 } from './neofamily_line2.js';
-import { tasks as t3 } from './neofamily_line3.js';
-import { tasks as t4 } from './neofamily_line4.js';
-import { tasks as t5 } from './neofamily_line5.js';
-import { tasks as t6 } from './neofamily_line6.js';
-import { tasks as t7 } from './neofamily_line7.js';
-import { tasks as t8 } from './neofamily_line8.js';
-import { tasks as t9 } from './neofamily_line9.js';
-import { tasks as t10 } from './neofamily_line10.js';
-import { tasks as t11 } from './neofamily_line11.js';
-import { tasks as t12 } from './neofamily_line12.js';
-import { tasks as t13 } from './neofamily_line13.js';
-import { tasks as t14 } from './neofamily_line14.js';
-import { tasks as t15 } from './neofamily_line15.js';
-import { tasks as t16 } from './neofamily_line16.js';
-import { tasks as t17 } from './neofamily_line17.js';
-import { tasks as t18 } from './neofamily_line18.js';
-import { tasks as t21 } from './neofamily_line21.js';
-import { tasks as t22 } from './neofamily_line22.js';
-import { tasks as t23 } from './neofamily_line23.js';
-import { tasks as t24 } from './neofamily_line24.js';
-import { tasks as t25 } from './neofamily_line25.js';
-import { tasks as t26 } from './neofamily_line26.js';
+import { tasks as t1 } from './ege_task1.js';
+import { tasks as t2 } from './ege_task2.js';
+import { tasks as t3 } from './ege_task3.js';
+import { tasks as t4 } from './ege_task4.js';
+import { tasks as t5 } from './ege_task5.js';
+import { tasks as t6 } from './ege_task6.js';
+import { tasks as t7 } from './ege_task7.js';
+import { tasks as t8 } from './ege_task8.js';
+import { tasks as t9 } from './ege_task9.js';
+import { tasks as t10 } from './ege_task10.js';
+import { tasks as t11 } from './ege_task11.js';
+import { tasks as t12 } from './ege_task12.js';
+import { tasks as t13 } from './ege_task13.js';
+import { tasks as t14 } from './ege_task14.js';
+import { tasks as t15 } from './ege_task15.js';
+import { tasks as t16 } from './ege_task16.js';
+import { tasks as t17 } from './ege_task17.js';
+import { tasks as t18 } from './ege_task18.js';
+import { tasks as t21 } from './ege_task21.js';
+import { tasks as t22 } from './ege_task22.js';
+import { tasks as t23 } from './ege_task23.js';
+import { tasks as t24 } from './ege_task24.js';
+import { tasks as t25 } from './ege_task25.js';
+import { tasks as t26 } from './ege_task26.js';
 
-const neofamilyData = {
-  ...t1, ...t2, ...t3, ...t4, ...t5, ...t6, ...t7, ...t8, ...t9, ...t10,
-  ...t11, ...t12, ...t13, ...t14, ...t15, ...t16, ...t17, ...t18,
-  ...t21, ...t22, ...t23, ...t24, ...t25, ...t26
+export const tasksDatabase = {
+  ...t1,
+  ...t2,
+  ...t3,
+  ...t4,
+  ...t5,
+  ...t6,
+  ...t7,
+  ...t8,
+  ...t9,
+  ...t10,
+  ...t11,
+  ...t12,
+  ...t13,
+  ...t14,
+  ...t15,
+  ...t16,
+  ...t17,
+  ...t18,
+  ...t21,
+  ...t22,
+  ...t23,
+  ...t24,
+  ...t25,
+  ...t26,
 };
-
-export const tasksDatabase = neofamilyData;
