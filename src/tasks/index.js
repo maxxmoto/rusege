cf5ae56728a559b6330e49a7ff99ed8a11609e89
maxxmoto <maxxmoto@users.noreply.github.com>
2026-06-27 00:44:50 +0300

    Restore neofamily line numbering as-is

--- src/tasks/index.js
+++ src/tasks/index.js
@@ -1,32 +1,32 @@
-import { tasks as t1 } from './neofamily_line1.js';
-import { tasks as t2 } from './neofamily_line2.js';
-import { tasks as t3 } from './neofamily_line3.js';
-import { tasks as t4 } from './neofamily_line4.js';
-import { tasks as t5 } from './neofamily_line5.js';
-import { tasks as t6 } from './neofamily_line6.js';
-import { tasks as t7 } from './neofamily_line7.js';
-import { tasks as t8 } from './neofamily_line8.js';
-import { tasks as t9 } from './neofamily_line9.js';
-import { tasks as t10 } from './neofamily_line10.js';
-import { tasks as t11 } from './neofamily_line11.js';
-import { tasks as t12 } from './neofamily_line12.js';
-import { tasks as t13 } from './neofamily_line13.js';
-import { tasks as t14 } from './neofamily_line14.js';
-import { tasks as t15 } from './neofamily_line15.js';
-import { tasks as t16 } from './neofamily_line16.js';
-import { tasks as t17 } from './neofamily_line17.js';
-import { tasks as t18 } from './neofamily_line18.js';
-import { tasks as t21 } from './neofamily_line21.js';
-import { tasks as t22 } from './neofamily_line22.js';
-import { tasks as t23 } from './neofamily_line23.js';
-import { tasks as t24 } from './neofamily_line24.js';
-import { tasks as t25 } from './neofamily_line25.js';
-import { tasks as t26 } from './neofamily_line26.js';
+import { tasks as l1 } from './neofamily_line1.js';
+import { tasks as l2 } from './neofamily_line2.js';
+import { tasks as l3 } from './neofamily_line3.js';
+import { tasks as l4 } from './neofamily_line4.js';
+import { tasks as l5 } from './neofamily_line5.js';
+import { tasks as l6 } from './neofamily_line6.js';
+import { tasks as l7 } from './neofamily_line7.js';
+import { tasks as l8 } from './neofamily_line8.js';
+import { tasks as l9 } from './neofamily_line9.js';
+import { tasks as l10 } from './neofamily_line10.js';
+import { tasks as l11 } from './neofamily_line11.js';
+import { tasks as l12 } from './neofamily_line12.js';
+import { tasks as l13 } from './neofamily_line13.js';
+import { tasks as l14 } from './neofamily_line14.js';
+import { tasks as l15 } from './neofamily_line15.js';
+import { tasks as l16 } from './neofamily_line16.js';
+import { tasks as l17 } from './neofamily_line17.js';
+import { tasks as l18 } from './neofamily_line18.js';
+import { tasks as l21 } from './neofamily_line21.js';
+import { tasks as l22 } from './neofamily_line22.js';
+import { tasks as l23 } from './neofamily_line23.js';
+import { tasks as l24 } from './neofamily_line24.js';
+import { tasks as l25 } from './neofamily_line25.js';
+import { tasks as l26 } from './neofamily_line26.js';
 
-const neofamilyData = {
-  ...t1, ...t2, ...t3, ...t4, ...t5, ...t6, ...t7, ...t8, ...t9, ...t10,
-  ...t11, ...t12, ...t13, ...t14, ...t15, ...t16, ...t17, ...t18,
-  ...t21, ...t22, ...t23, ...t24, ...t25, ...t26
+const linesMap = {
+  ...l1, ...l2, ...l3, ...l4, ...l5, ...l6, ...l7, ...l8, ...l9, ...l10,
+  ...l11, ...l12, ...l13, ...l14, ...l15, ...l16, ...l17, ...l18,
+  ...l21, ...l22, ...l23, ...l24, ...l25, ...l26
 };
 
-export const tasksDatabase = neofamilyData;
+export const tasksDatabase = linesMap;
